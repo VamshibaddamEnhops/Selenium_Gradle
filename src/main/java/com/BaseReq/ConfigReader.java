2fbdacdb3472f34dc1b1dbe840801ca763d7738d
VamshibaddamEnhops <vamshidharb@proarch.com>
2025-05-21 16:34:58 +0530

    Update ConfigReader.java

--- src/main/java/com/BaseReq/ConfigReader.java
+++ src/main/java/com/BaseReq/ConfigReader.java
@@ -1,29 +1,3 @@
-// package com.BaseReq;
-
-// import java.io.FileInputStream;
-// import java.io.IOException;
-// import java.util.Properties;
-
-// public class ConfigReader {
-//     private static Properties properties;
-
-//     static {
-//         try {
-//             properties = new Properties();
-//             FileInputStream fis = new FileInputStream("src/test/resources/config/config.properties");
-//             properties.load(fis);
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//             throw new RuntimeException("config.properties file not found or invalid");
-//         }
-//     }
-
-//     public static String get(String key) {
-//         return properties.getProperty(key);
-//     }
-// }
-
-
 package com.BaseReq;
 
 import java.io.FileInputStream;
